85bb4506b52baaee7e5ef2c8672ab513585d6268
naufaldzakwannn <naufaldzakwan189@gmail.com>
2026-07-23 22:38:33 +0700

    Scale Lightstick

--- app/components/Lightstick/Lightstick.tsx
+++ app/components/Lightstick/Lightstick.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./Lightstick.module.css";
 
 export default function Lightstick() {
+  const [isOn, setIsOn] = useState(false);
+
   return (
     <div className={styles.wrapper}>
-      <Image src="/images/lightstick/lightstick.svg" alt="Lightstick" width={90} height={180} className={styles.lightstick} priority />
+      <button className={`${styles.button} ${isOn ? styles.on : ""}`} onClick={() => setIsOn(!isOn)} aria-label="Toggle Lightstick">
+        <Image src="/images/lightstick/lightstick.svg" alt="Lightstick" width={90} height={180} priority className={styles.lightstick} />
+      </button>
     </div>
   );
 }
